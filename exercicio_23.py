17ca8fd4e9c9a682d81c347a0d02e601402d00d0
Linho <140359144+PectylsonLinho@users.noreply.github.com>
2023-08-06 23:49:47 +0100

    Create exercicio_23.py
    Exercitando a separação de dígitos de um numero!

---
+++ exercicio_23.py
@@ -0,0 +1,15 @@
+header = 'read a number, range(0- 9999)'
+print('{:=^40}'.format(header))
+
+number = int(input('Digite um numero: '))
+num = number // 1 % 10
+num1 = number // 10 % 10
+num2 = number // 100 % 10
+num3 = number // 1000 % 10
+print('Analisando o valor!')
+print('''
+       Unidade: {}
+       Dezena:  {}
+       Centena: {}
+       Milhar:  {}
+'''.format(num, num1, num2, num3))
